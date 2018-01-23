ed0236a10a90add4acdca12da690cf9584bd06e9
atafoabure <atafoa@yahoo.com>
2018-01-22 22:43:03 -0600

    Initial commit of Homework 2

---
+++ Homework 2/Homework 2/aaa2575_Secret_Code_1.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Homework 2
+//
+//  Created by Atafo Abure on 1/22/18.
+//  Copyright © 2018 Atafo Abure. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
