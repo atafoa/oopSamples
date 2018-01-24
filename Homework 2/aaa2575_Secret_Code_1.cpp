ae2d8f2c8a05219fe4867b3e14116dfd32c07ba2
Atafo Abure <atafoa@yahoo.com>
2018-01-23 18:23:39 -0600

    Final

---
+++ Homework 2/aaa2575_Secret_Code_1.cpp
@@ -0,0 +1,79 @@
+//
+//  secret_code_1.cpp
+//  Homework 2
+//
+//  Created by Atafo Abure on 1/22/18.
+
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <ctype.h>
+#include <stdio.h>
+
+
+using namespace std;
+
+int main()
+
+{
+	// Open the file to be read
+	ifstream myInputFile ("Words.txt"); // creates or opens the input file
+	string line;
+	vector<char> secretCode;
+	string str;
+	int secretNumber = 0;
+	vector<string>  words;
+	char c;
+	
+	if(myInputFile.is_open())
+	{
+		while(getline (myInputFile, line))
+		{
+			//store each words in vector
+			words.push_back(line);
+			cout << line << endl;
+		}
+		
+		
+		// iterates through vector and checks for uppercase letters
+		for(int i = 0; i < words.size(); i++)
+		{
+			str = words[i];
+			for (int j = 0; j < str.size(); j++)
+			{
+				c = str[j];
+				if(isupper(c))
+				{
+					secretCode.push_back(c);
+				}
+				
+				if(isdigit(c))
+				{
+					secretNumber += (c -'0');
+				}
+			}
+			
+		}
+
+		//Append to file
+		ofstream outfile;
+		outfile.open("Words.txt",ofstream::app);
+
+		cout << "\n" << "The Secret Code is: ";
+		outfile << "\n\n" << "The Secret Code is: ";
+		for(int k = 0; k < secretCode.size(); k++)
+		{
+			cout << secretCode[k];
+			outfile << secretCode[k];
+			
+		}
+		cout << secretNumber << endl;
+		outfile << secretNumber << endl;
+
+		myInputFile.close();
+	}
+	else
+		cout << "Unable to open file" << endl;
+	return 0;
+}
