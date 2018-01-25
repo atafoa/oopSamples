a1df663c06ae006f30ae4d6969bb725da55053c3
atafoabure <atafoa@yahoo.com>
2018-01-24 19:19:27 -0600

    Finished Project

---
+++ Homework 2/aaa2575_Secret_Code_1.cpp
@@ -0,0 +1,61 @@
+//
+//  secret_code_1.cpp
+//  Homework 2
+//
+//  Created by Atafo Abure on 1/22/18.
+
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <ctype.h>
+#include <stdio.h>
+
+
+using namespace std;
+
+int main()
+
+{
+	// Open the file to be read
+	ifstream myInputFile ("Words.txt"); // creates or opens the input file
+	string line;
+	string secretCode;
+	string str;
+	double sumOfNumbers;
+	vector<string>  words;
+	char c;
+	
+	if(myInputFile.is_open())
+	{
+		while(getline (myInputFile, line))
+		{
+			//store each words in vector
+			words.push_back(line);
+			cout << line << endl;
+		}
+		
+		
+		// iterates through vector and checks for uppercase letters
+		for(int i = 0; i < words.size(); i++)
+		{
+			str = words[i];
+			for (int j = 0; j < str.size(); j++)
+			{
+				c = str[j];
+				if(isupper(c))
+				{
+					secretCode = c;
+				}
+			}
+			
+		}
+		
+		cout << secretCode << endl;
+		
+		myInputFile.close();
+	}
+	else
+		cout << "Unable to open file" << endl;
+	return 0;
+}
