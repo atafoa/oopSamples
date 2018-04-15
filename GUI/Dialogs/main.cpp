b0a906cd9b908dcf3dbff7f288ea985d81782e67
Atafo Abure <atafoa@yahoo.com>
2018-04-15 00:33:48 -0500

    Updated all errors

--- GUI/Dialogs/main.cpp
+++ GUI/Dialogs/main.cpp
@@ -7,6 +7,7 @@ int main (int argc, char* argv[])
     
     //Dialogs::message("test", "Title");
     //std::cout << Dialogs::input("What's your name?", "Question") << std::endl;
+    std::vector<std::string> buttons = {"No", "Yes", "Maybe"};
     std::cout << Dialogs::question("does this work?", "Test", buttons) << std::endl; 
     return 0;
 }
